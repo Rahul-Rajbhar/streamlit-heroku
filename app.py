1b5a91641ce1079e38a08abc189743ab622c0d60
Rahul-Rajbhar <84897451+Rahul-Rajbhar@users.noreply.github.com>
2022-02-07 22:52:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
 from PIL import Image
-
-def app():
     
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
